56a54558553ceffff8dbc8c8e846a3bbaddfabcd
Danny Hanson <hansond@carleton.edu>
2014-11-02 16:51:55 -0600

    One person tracking successful, not two

--- Tracking/architecture.py
+++ Tracking/architecture.py
@@ -6,9 +6,9 @@ import os
 class Face:
 	def __init__(self, useFeatures=False):
 		# MAYBE BOUNDING BOX?
-		self.position = [(),(), dt.datetime.now()]		# where the face is in the frame
+		self.position = []		# where the face is in the frame
 		self.prevPositions = []		# all previous positions
-		self.velocity = (0,0)		# vector describing x change, y change in pixels per frame
+		self.velocity = (0,0,0)		# vector describing x change, y change, and time change
 		self.orientation = 0		# the tilt of the head (degrees)
 		self.timeSinceDetection = 0	# frames since last detection
 		self.obscured = False		# whether the head was detected in the last detection
@@ -25,19 +25,19 @@ class Face:
 	def getPosition(self):
 		return self.position
 
-	def setPosition(self, x1, y1, x2, y2):
+	def setPosition(self, rects):
 		"""takes list of tuples pos and sets that as position
 		and updates prevPositions. Also sets velocity"""
-		if (x1==None):
+		if (rects==[]):
 			self.obscured = True
 		else:
 			self.prevPositions.append(self.position)
-			self.position = [(x1,y1),(x2,y2),dt.datetime.now()]
+			self.position = [(rects[0],rects[1]),(rects[2],rects[3]),dt.datetime.now()]
 			self.timeSinceDetection = 0
 
 	def getSize(self):
 		"""using position, return size of face"""
-		if (self.position==[(),()]):
+		if (self.position==[]):
 			return 0
 		return abs((self.position[1][0]-self.position[0][0])*(self.position[1][1]-self.position[0][1]))
 
@@ -60,10 +60,12 @@ class Face:
 	def getColorAvg(self):
 		return self.colorAvg
 
-	def estimateNextPosition(selfself):
+	def estimateNextPosition(self, time):
 		"""Use velocity and most recent position
 		to calculate a new slightly modified position. Meant for when
 		we are estimating where the face is"""
+		self.prevPositions.append(self.position)
+		self.position[0]
 		pass
 
 	def calculateVelocity(self):
@@ -75,7 +77,8 @@ class Face:
 			time = self.position[2] - self.prevPositions[len(self.prevPositions)-1][2]
 			xdist = self.position[0][0] - self.prevPositions[len(self.prevPositions)-1][0][0]
 			ydist = self.position[0][1] - self.prevPositions[len(self.prevPositions)-1][0][1]
-			self.velocity = math.pow(math.pow(1.0*xdist,2) + math.pow(1.0*ydist,2),0.5) / (1.0*time.total_seconds())
+			#self.velocity = math.pow(math.pow(1.0*xdist,2) + math.pow(1.0*ydist,2),0.5) / (1.0*time.total_seconds())
+			self.velocity = (xdist,ydist,time.total_seconds())
 
 	def getVelocity(self):
 		return self.velocity
@@ -92,6 +95,10 @@ class Face:
 		is limited to the facial area"""
 		pass
 
+	def likelihoodOfBeingHere(self, location, time):
+		"""checks what the chance of the face being at this location is"""
+		pass
+
 class Video:
 	def __init__(self, vidSource, frameGap=0):
 		self.vidcap = cv2.VideoCapture(vidSource)
@@ -105,11 +112,94 @@ class Video:
 		self.frameImage = None          # this is whatever kind of image returned by openCV
 		# SOME SORT OF VIDEO OBJECT
 		cv2.namedWindow("show")
+		self.colors = [(255,0,0), (0,255,0), (0,0,255)]
 
 
-	def addNewFace(self):
+	def analyzeFrame(self,rects):
+		if len(rects)>len(self.visibleFaceList):
+			megaList = []
+			for i in range(len(self.visibleFaceList)):
+				tempList = []
+				for j in range(len(rects)):
+					tempList.append(self.likelihoodOfBeingHere(self.visibleFaceList[i],rects[j]))
+				# if there are issues, it's with copying
+				megaList.append(list(tempList))
+
+			assignmentList = []
+			while len(assignmentList) < len(self.visibleFaceList):
+				highestVal = 0
+				highestIndex = ()
+				for i in range(len(megaList)):
+					for j in range(len(megaList[i])):
+						if j not in assignmentList:
+							if megaList[i][j] >= highestVal:
+								highestVal = megaList[i][j]
+								highestIndex = (i, j)
+				assignmentList.append(j)
+			self.makeAssignments(assignmentList, rects)
+
+			for i in range(len(rects)):
+				if i not in assignmentList:
+					fc = Face()
+					fc.setPosition(rects[i])
+					self.visibleFaceList.append(fc)
+
+
+		elif len(rects)==len(self.visibleFaceList):
+			megaList = []
+			for i in range(len(self.visibleFaceList)):
+				tempList = []
+				for j in range(len(rects)):
+					tempList.append(self.likelihoodOfBeingHere(self.visibleFaceList[i],rects[j]))
+				# if there are issues, it's with copying
+				megaList.append(list(tempList))
+
+			assignmentList = []
+			while len(assignmentList) < len(rects):
+				highestVal = 0
+				highestIndex = ()
+				for i in range(len(megaList)):
+					for j in range(len(megaList[i])):
+						if j not in assignmentList:
+							if megaList[i][j] >= highestVal:
+								highestVal = megaList[i][j]
+								highestIndex = (i, j)
+				assignmentList.append(j)
+			self.makeAssignments(assignmentList, rects)
+
+		else:		# more rects than faces
+			pass
+
+	def makeAssignments(self, assignmentList, rects):
+		for i in range(len(self.visibleFaceList)):
+			self.visibleFaceList[i].setPosition(rects[assignmentList[i]])
+
+
+	def addNewFace(self, fc):
 		"""analyzes movement of a new face to preserve effeciency"""
-		pass
+		self.visibleFaceList.append(fc)
+
+	def likelihoodOfBeingHere(self, face1, rect):
+		"""compares to faces and sees what the chances are that these faces are the same"""
+		"""returns flot between 0 and 1"""
+		# expanding circle
+		time = dt.datetime.now()
+		recentPosition = face1.getPosition()
+		if not (recentPosition==[]):
+			deltaTime = (time - recentPosition[2]).total_seconds()
+			# eventually depend on size of face, for now straight up linear
+			size = math.pow(face1.getSize(),0.5)
+			radius = deltaTime*size
+			middleOfRect = ((rect[2]+rect[0])/2,(rect[3]+rect[1])/2)
+			middleOfFace = ((recentPosition[1][0]+recentPosition[0][0])/2,(recentPosition[1][1]+recentPosition[0][1])/2)
+			diffMiddles = math.pow(math.pow(middleOfFace[0]-middleOfRect[0], 2) + math.pow(middleOfFace[1]-middleOfRect[1], 2), 0.5)
+			prob = 1-(diffMiddles/radius)
+			if prob > 0:
+				return prob
+			else:
+				return 0
+		else:
+			return 0
 
 	def readFrame(self):
 		"""read frame from openCV info"""
@@ -130,6 +220,8 @@ class Video:
 		
 		Should be run every frame except where detectAll() is run."""
 		pass
+		# for face in self.visibleFaceList[]:
+		# 	face.estimateNextPosition()
 
 
 	def step(self):
@@ -141,10 +233,13 @@ class Video:
 
 	def display(self):
 		""" Displays current frame, as well as objects associated with faces"""
-		cv2.imshow("show", self.frameImage)
+		print len(self.visibleFaceList)
+		for i in range(len(self.visibleFaceList)):
+			self.showRectangle(self.visibleFaceList[i].getPosition(),self.colors[i])
+			cv2.imshow("show", self.frameImage)
 
-	def showRectangle(self, pos):
-		cv2.rectangle(self.frameImage, pos[0], pos[1], (127, 255, 0), 2)
+	def showRectangle(self, pos, color):
+		cv2.rectangle(self.frameImage, pos[0], pos[1], color, 2)
 		
 	def endWindow(self):
 		self.vidcap.release()
--- Tracking/livetrackwithface.py
+++ Tracking/livetrackwithface.py
@@ -4,27 +4,24 @@ from architecture import Face
 from architecture import Video
 
 def goGetEm():
-	fc = Face()
 	vid = Video(0)
 	vid.readFrame()
 	while (True):
 		#detect
 		rects = vid.detectAll()
 		if len(rects)==0:
-			fc.setPosition(None,1,1,1)
-			fc.incrementTimeSinceDetection()
+			# fc.setPosition()
+			# fc.incrementTimeSinceDetection()
 			rects = []
 		else:
 			rects[:, 2:] += rects[:, :2]
 		#make box image
-		#print fc.getTimeSinceDetection()
-		for x1, y1, x2, y2 in rects:
-			fc.setPosition(x1, y1, x2, y2)
-			pos = fc.getPosition()
-			fc.calculateVelocity()
-			print fc.getVelocity()
-			# print pos
-			vid.showRectangle(pos)
+		vid.analyzeFrame(rects)
+		# for rect in rects:
+		# 	fc.setPosition(rect)
+		# 	pos = fc.getPosition()
+		# 	fc.calculateVelocity()
+		# 	vid.showRectangle(pos)
 		vid.display()
 		vid.readFrame()				#read next frame
 		key = cv2.waitKey(20)
