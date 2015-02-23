b624c0d9c9367f5f0b2c9446348a2dc3bfba521d
katjacollier <collierk@carleton.edu>
2015-02-22 22:03:23 -0600

    Made groucho glasses demo work better

--- groucho-glasses-demo/face-tracking-test.py
+++ groucho-glasses-demo/face-tracking-test.py
@@ -8,7 +8,7 @@ glasses = Image.open('groucho_glasses.png', 'r')
 face_cascade = cv2.CascadeClassifier("face_cascade2.xml")
 
 def detect_img(img):
-    rects = face_cascade.detectMultiScale(img, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (20,20))
+    rects = face_cascade.detectMultiScale(img, 1.3, 5, cv2.cv.CV_HAAR_SCALE_IMAGE, (20,20))
     if len(rects) == 0:
         return [], img
     rects[:, 2:] += rects[:, :2]
@@ -17,8 +17,8 @@ def detect_img(img):
     
 def draw_things_2(rects, face_pic):
 	for (x1, y1, x2, y2) in rects:
-		rescaled_glasses = glasses.resize((x2 - x1, y2 - y1))
-		face_pic.paste(rescaled_glasses, (x1, y1), rescaled_glasses)
+		rescaled_glasses = glasses.resize((x2 - x1, int((y2 - y1)*.75)))
+		face_pic.paste(rescaled_glasses, (x1, y1+int((y2 - y1)*.1)), rescaled_glasses)
 	return face_pic
     
 
@@ -32,15 +32,16 @@ def capture_video():
 		# Our operations on the frame come here
 		#gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 		gray = frame
+		frameImage = cv2.flip(gray,1)
 		
-		rects, img = detect_img(gray)
+		rects, img = detect_img(frameImage)
 		#box(rects, img)
 		#gray = list(.getdata())
-		gray = np.asarray(draw_things_2(rects, Image.fromarray(gray)))
+		frameImage = np.asarray(draw_things_2(rects, Image.fromarray(frameImage)))
 		#print gray
 
 		# Display the resulting frame
-		cv2.imshow('frame',gray)
+		cv2.imshow('frame',frameImage)
 		
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
